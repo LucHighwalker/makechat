4c8aa70495a2328818f429877e33f60a46b499ec
LucHighwalker <luc.highwalker@gmail.com>
2019-02-06 12:52:14 -0800

    users can join and leave

--- public/index.js
+++ public/index.js
@@ -1,12 +1,20 @@
-$(document).ready(()=>{
+$(document).ready(() => {
   const socket = io.connect();
-
-  //Keep track of the current user
   let currentUser;
+  // Get the online users from the server
+  socket.emit('get online users');
 
-  $('#createUserBtn').click((e)=>{
+  socket.on('get online users', onlineUsers => {
+    //You may have not have seen this for loop before. It's syntax is for(key in obj)
+    //Our usernames are keys in the object of onlineUsers.
+    for (username in onlineUsers) {
+      $('.usersOnline').append(`<p class="userOnline">${username}</p>`);
+    }
+  });
+
+  $('#createUserBtn').click(e => {
     e.preventDefault();
-    if($('#usernameInput').val().length > 0){
+    if ($('#usernameInput').val().length > 0) {
       socket.emit('new user', $('#usernameInput').val());
       // Save the current user when created
       currentUser = $('#usernameInput').val();
@@ -15,34 +23,40 @@ $(document).ready(()=>{
     }
   });
 
-  $('#sendChatBtn').click((e) => {
+  $('#sendChatBtn').click(e => {
     e.preventDefault();
     // Get the message text value
     let message = $('#chatInput').val();
     // Make sure it's not empty
-    if(message.length > 0){
+    if (message.length > 0) {
       // Emit the message with the current user to the server
       socket.emit('new message', {
-        sender : currentUser,
-        message : message,
+        sender: currentUser,
+        message: message
       });
-      $('#chatInput').val("");
+      $('#chatInput').val('');
     }
   });
 
   //socket listeners
-  socket.on('new user', (username) => {
+  socket.on('new user', username => {
     console.log(`${username} has joined the chat`);
     $('.usersOnline').append(`<div class="userOnline">${username}</div>`);
-  })
+  });
 
-  socket.on('new message', (data) => {
+  socket.on('new message', data => {
     $('.messageContainer').append(`
       <div class="message">
         <p class="messageUser">${data.sender}: </p>
         <p class="messageText">${data.message}</p>
       </div>
     `);
-  })
+  });
 
-})
+  socket.on('user has left', onlineUsers => {
+    $('.usersOnline').empty();
+    for (username in onlineUsers) {
+      $('.usersOnline').append(`<p>${username}</p>`);
+    }
+  });
+});
